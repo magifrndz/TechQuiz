673072593d5a13c47fa90276be8bb34b2fbfc3dd
magi <kumar.magesh554@gmail.com>
2017-12-17 04:52:42 +0530

    Login Form Designed

--- app/src/main/java/com/magesh/techquiz/MainActivity.java
+++ app/src/main/java/com/magesh/techquiz/MainActivity.java
@@ -3,11 +3,18 @@ package com.magesh.techquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       /* CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("font/alex.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );*/
     }
 }
